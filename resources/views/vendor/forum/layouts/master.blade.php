c5c69d0781f460bf5f10524339fd9de06fc4aa38
Rick Mann <hello@ricko.me>
2015-04-23 23:48:50 +0100

    Fix data-confirm handling

--- resources/views/vendor/forum/layouts/master.blade.php
+++ resources/views/vendor/forum/layouts/master.blade.php
@@ -6,17 +6,20 @@
 
 @section('bottom')
 <script>
-$('[data-method]:not(.disabled)').click(function(event) {
-	$('<form action="' + $(this).attr('href') + '" method="POST">' +
-	'<input type="hidden" name="_method" value="' + $(this).data('method') + '">' +
-	'<input type="hidden" name="_token" value="{!! Session::getToken() !!}"' +
-	'</form>').submit();
-
-	event.preventDefault();
+$('a[data-confirm]').click(function(event) {
+    if (!confirm('{!! trans('forum::base.generic_confirm') !!}')) {
+        event.stopImmediatePropagation();
+        event.preventDefault();
+    }
 });
 
-$('a[data-confirm]').click(function() {
-	return confirm('{!! trans('forum::base.generic_confirm') !!}');
+$('[data-method]:not(.disabled)').click(function(event) {
+    $('<form action="' + $(this).attr('href') + '" method="POST">' +
+    '<input type="hidden" name="_method" value="' + $(this).data('method') + '">' +
+    '<input type="hidden" name="_token" value="{!! Session::getToken() !!}"' +
+    '</form>').submit();
+
+    event.preventDefault();
 });
 </script>
 @stop
