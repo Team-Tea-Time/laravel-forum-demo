f3b2cdbf0baac5bfcb08123516e760e256a17aad
Riari <3583774+Riari@users.noreply.github.com>
2021-11-18 21:56:49 +0000

    Add bottom margin to footer

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -235,7 +235,7 @@
 
     <div class="mask"></div>
 
-	<div class="text-center text-muted">
+	<div class="text-center text-muted mb-3">
 		Running Laravel Forum 5 on Laravel 8.<br>
 		See <a href="https://github.com/Team-Tea-Time/laravel-forum-demo">demo source</a>, <a href="https://github.com/Team-Tea-Time/laravel-forum">package source</a>, and <a href="https://www.teamteatime.net/docs/laravel-forum/5/">docs</a>.<br>
 		Resets weekly on Sundays.
