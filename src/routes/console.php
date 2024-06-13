04523f3155fe021e89415cb0ddb1f725a098be43
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-06-13 21:51:08 +0100

    Schedule the demo reset command

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\ResetDemo;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command(ResetDemo::class)->weekly();
